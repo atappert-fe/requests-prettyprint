1bc0e6c716f1a20c0b8530d3ccc30c14ecb4ce93
Andrew Tappert <andrew.tappert@fireeye.com>
2020-09-29 20:13:32 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+ 
+setuptools.setup(
+    name="Python Requests Pretty Printer",
+    version="0.0.1",
+    author="Andrew Tappert",
+    url="https://github.com/atappert-fe/requests-prettyprint",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
